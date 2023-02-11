a21251b4a6393e56d1ef6d7c6181924007b5f6dc
Michael Pace <michael.g.pace@gmail.com>
2023-02-11 20:59:30 +0000

    basic requires and status filled in server file

--- server.js
+++ server.js
@@ -11,3 +11,20 @@ app.use(cors());
 const mongoose = require("mongoose");
 
 mongoose.connect(process.env.DB_URL);
+
+const PORT = process.env.PORT || 3002;
+
+app.get("/", (request, response) => {
+  response.status(200).send("Hi from the server!");
+});
+
+app.get("*", (request, response) => {
+  response.statue(404).send("Not available");
+});
+
+// eslint-disable-next-line no-unused-vars
+app.use((error, request, response, next) => {
+  response.status(500).send(error.message);
+});
+
+app.listen(PORT, () => console.log(`listening on Port ${PORT}`));
